a8f2eee6a70e4aa7735143932b0cb208dd9bf5b3
JOSERICARDO-2025 <josericardorecenosgonzalez@gmail.com>
2025-11-11 11:09:42 -0600

    Create Taller.kt
    taller

---
+++ app/src/main/java/com/example/pca/Taller.kt
@@ -0,0 +1,7 @@
+// Data class para representar cada taller
+data class Taller(
+    val nombre: String,
+    val cursosDisponibles: Int,
+    // ID del recurso drawable (ej. R.drawable.ic_taller_dibujo)
+    val iconoResId: Int
+)
